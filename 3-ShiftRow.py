5159cd40d70e3e6f4d6f1b5ad32df18e3f0dd538
1637497 <91468702+1637497@users.noreply.github.com>
2024-03-09 18:17:48 +0100

    Update 3-ShiftRow.py

--- 3-ShiftRow.py
+++ 3-ShiftRow.py
@@ -1,15 +1,13 @@
-def ShiftRow(m):
+def shiftrow(m):
     print('Entrada:', m)
     #segona fila
     aux = m[1,0]
     m[1,0:3] = m[1,1:4]
     m[1,3] = aux
-
     #terceta fila
     m2 = list(m[2])
     m2[0:2], m2[2:4] = m2[2:4], m2[0:2]
     m[2][:] = m2
-
     #quarta fila
     aux = m[3,3]
     m[3,1:4] = m[3,0:3]
