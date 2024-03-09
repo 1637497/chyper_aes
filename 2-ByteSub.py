d0cd30d9841152dbaeece1f6a957323c90ec303f
1637497 <91468702+1637497@users.noreply.github.com>
2024-03-09 14:20:03 +0100

    Update 2-ByteSub.py

--- 2-ByteSub.py
+++ 2-ByteSub.py
@@ -1,7 +1,9 @@
 def ByteSub(sbox, text):
+    print('Entrada:', text)
     matriu = np.zeros((4,4))
     for i in range(4):
       for j in range(4):
         valor = text[i][j]
         matriu[i][j] = sbox[int(valor[0], 16)][int(valor[1], 16)]
+    print('Sortida:', matriu)
     return matriu
