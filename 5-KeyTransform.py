25644d2644481a81f375648f5af4bbccd86876b6
1637497 <91468702+1637497@users.noreply.github.com>
2024-03-09 18:03:05 +0100

    Update 5-KeyTransform.py

--- 5-KeyTransform.py
+++ 5-KeyTransform.py
@@ -5,8 +5,8 @@ def key_expansion(clau, sbox, racon):
         for i in range(4):
             valor = hex(ultima_col[i])[2:].zfill(2)
             ultima_col[i] = sbox[int(valor[0], 16)][int(valor[1], 16)]
-        for col in range(4):
-            clau[:, col] = np.bitwise_xor(clau[:, col], ultima_col)
-        clau[:, 0] =  np.bitwise_xor(clau[:, 0], racon[:, iteracio])
+        clau[:, 0] = np.bitwise_xor(clau[:, 0], np.bitwise_xor(ultima_col, racon[:, iteracio]))
+        for col in range(1, 4):
+            clau[:, col] = np.bitwise_xor(clau[:, col], clau[:, col-1])
         claus.append(clau.copy())
     return claus
