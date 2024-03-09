7ab169df06a17e54d31e153210b4e70695de85c8
1637497 <91468702+1637497@users.noreply.github.com>
2024-03-09 13:33:48 +0100

    Create ByteSub.py

---
+++ ByteSub.py
@@ -0,0 +1,7 @@
+def ByteSub(sbox, text):
+    matriu = np.zeros((4,4))
+    for i in range(4):
+      for j in range(4):
+        valor = text[i][j]
+        matriu[i][j] = sbox[int(valor[0], 16)][int(valor[1], 16)]
+    return matriu
