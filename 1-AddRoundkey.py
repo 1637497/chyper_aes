2c014499d455666d282bef9b16a74f4397047e93
1637497 <91468702+1637497@users.noreply.github.com>
2024-03-09 14:19:16 +0100

    Update 1-AddRoundkey.py

--- 1-AddRoundkey.py
+++ 1-AddRoundkey.py
@@ -1,2 +1,5 @@
 def addroundkey(matriu, round_key):
-    return np.bitwise_xor(matriu, round_key)
+    print('Entrada:', matriu)
+    state = np.bitwise_xor(matriu, round_key)
+    print('Sortida:', state)
+    return state
